73008c0afc2d3d8c5dedc0e10ba9fc38cad57872
Nick Marucci <npm1514@gmail.com>
2016-08-08 15:48:21 -0600

    styled page

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var width = 960,
-    height = 500;
+    height = 600;
 
 var projection = d3.geo.albers()
     .rotate([96, 0])
     .center([-.6, 38.7])
     .parallels([29.5, 45.5])
-    .scale(1070)
+    .scale(1170)
     .translate([width / 2, height / 2])
     .precision(.1);
 
@@ -16,7 +16,7 @@ var graticule = d3.geo.graticule()
     .extent([[-98 - 45, 38 - 45], [-98 + 45, 38 + 45]])
     .step([5, 5]);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".usa").append("svg")
     .attr("width", width)
     .attr("height", height);
 
@@ -33,15 +33,26 @@ d3.json("usa.json", function(error, us) {
       .attr("class", "land")
       .attr("d", path);
 
-  svg.insert("path", ".graticule")
-      .datum(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && !(a.id / 1000 ^ b.id / 1000); }))
-      .attr("class", "county-boundary")
-      .attr("d", path);
+  svg.append("g")
+      .attr("class", "counties")
+      .selectAll("path")
+      .data(topojson.feature(us, us.objects.counties).features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("fill", function(){
+        return d3.hsl(Math.random()*360, 0.5, 0.5);
+      });
 
-  svg.insert("path", ".graticule")
-      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
-      .attr("class", "state-boundary")
-      .attr("d", path);
+  svg.append("g")
+      .attr("class", "states")
+      .selectAll("path")
+      .data(topojson.feature(us, us.objects.states).features)
+      .enter()
+      .append("path")
+      .attr("d", path)
+      .attr("fill", "none")
+      .attr("stroke", "white");
 });
 
 d3.select(self.frameElement).style("height", height + "px");
